df98c8dab3a561919ce0dd3a77e1c171088f79cf
cmosmediatech <153702262+cmosmediatech@users.noreply.github.com>
2024-03-21 15:16:11 +0900

    Update usb_serial.mk

--- package/rockchip/usb_serial/usb_serial.mk
+++ package/rockchip/usb_serial/usb_serial.mk
@@ -11,6 +11,7 @@ define USB_SERIAL_INSTALL_STAGING_CMDS
 	mkdir -p $(TARGET_DIR)/lib/modules/
 	$(INSTALL) -D -m 0644 $(TOPDIR)/../kernel/drivers/usb/serial/usbserial.ko $(TARGET_DIR)/lib/modules/usbserial.ko
 	$(INSTALL) -D -m 0755 $(TOPDIR)/../app/usb_serial/S99_load_usbserial_modules $(TARGET_DIR)/etc/init.d/S99_load_usbserial_modules
+        $(INSTALL) -D -m 0755 $(TOPDIR)/../app/dc100_test/S99_start_netserver $(TARGET_DIR)/etc/init.d/S99_start_netserver
 endef
 
 $(eval $(generic-package))
